8c5f3d873d3667b199bb5b006445d20fa32ec299
jciechowski <jciechowski@gmail.com>
2014-04-02 19:25:53 +0200

    reading file

--- NumericalIntegration/numerical_integration.cpp
+++ NumericalIntegration/numerical_integration.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string FUN = "";
+string FILEPATH = "";
+
 int main(int argc, char** argv){
+	int i = 0;
+	while (i < argc){
+		if (!strcmp(argv[i], "-fun")) FUN = argv[++i];
+		if (!strcmp(argv[i], "-f")) FILEPATH = argv[++i];
+		i++;
+	}
+	cout << FILEPATH << endl;
 	return 0;
 }
