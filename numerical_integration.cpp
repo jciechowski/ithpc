198fc576ff4fd24b2ea2eb13e9585a559a403930
jciechowski <sianex@gmail.com>
2014-04-03 19:24:27 +0200

    reading from file and function parsing

--- numerical_integration.cpp
+++ numerical_integration.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <math.h>
 #include "timers.h"
+#include "exprtk.hpp"
 
 using namespace std;
 
@@ -10,26 +12,39 @@ string FILEPATH = "";
 int N = 500;
 
 
-double f(double x) { return cos(x); }
+// double f(double x) { return cos(x); }
 
+template<typename T>
+double f(T x){
+  exprtk::symbol_table<T> symbol_table;
+  symbol_table.add_variable("x",x);
+  symbol_table.add_constants();
+
+  exprtk::expression<T> expression;
+  expression.register_symbol_table(symbol_table);
+
+  exprtk::parser<T> parser;
+  parser.compile(FUN,expression);
+  return expression.value();
+}
 void simpsonRule() {
   double x1,x2,result,t,dx,x;
   x1 = 0; x2 = 1; 
   result = 0; t = 0;
 
   dx = (x2 - x1) / N;
-  pTimer zegar = newTimer(); // make new timer
-  startTimer(zegar); // store first timestamp
+  pTimer zegar = newTimer(); 
+  startTimer(zegar); 
   for(int i = 1; i <= N; i++)
     {
       x = x1 + i * dx;
-      t += f(x - dx / 2);
+      t += f<double>(x - dx / 2);
       if(i < N) result += f(x);
     }
-  result = dx / 6 * (f(x1) + f(x2) + 2 * result + 4 * t);
-  stopTimer(zegar);  // store second timestamp
-  printTimer(zegar); // show time difference
-  freeTimer(zegar);  // free occupied ram
+  result = dx / 6 * (f<double>(x1) + f<double>(x2) + 2 * result + 4 * t);
+  stopTimer(zegar);  
+  printTimer(zegar); 
+  freeTimer(zegar);  
   cout << endl << "Wynik calkowania metoda Simpsona: " << result << endl;
 }
 void rectangleRule() {
@@ -41,16 +56,15 @@ void rectangleRule() {
   h = (x2 - x1) / (double)N;
 
   result = 0;
-  pTimer zegar = newTimer(); // make new timer
-  startTimer(zegar); // store first timestamp
+  pTimer zegar = newTimer(); 
+  startTimer(zegar); 
   for (int i = 1; i <= N; i++)
-    result += f(x1 + i*h)*h;
-  stopTimer(zegar);  // store second timestamp
-  printTimer(zegar); // show time difference
-  freeTimer(zegar);  // free occupied ram
+    result += f<double>(x1 + i*h)*h;
+  stopTimer(zegar);  
+  printTimer(zegar); 
+  freeTimer(zegar);  
   cout << endl << "Wynik calkowania metoda prostokatow: " << result << endl;
 }
-
 void trapezoidalRule(){
   double x1, x2, h, result;
 
@@ -60,19 +74,26 @@ void trapezoidalRule(){
   h = (x2 - x1) / (double)N;
 
   result = 0;
-  pTimer zegar = newTimer(); // make new timer
-  startTimer(zegar); // store first timestamp
+  pTimer zegar = newTimer(); 
+  startTimer(zegar); 
   for (int i = 1; i<N; i++)
-    {
-      result += f(x1 + i * h);
-    }
-  result += f(x1) / 2;
-  result += f(x2) / 2;
+      result += f<double>(x1 + i * h);
+  result += f<double>(x1) / 2;
+  result += f<double>(x2) / 2;
   result *= h;
-  stopTimer(zegar);  // store second timestamp
-  printTimer(zegar); // show time difference
-  freeTimer(zegar);  // free occupied ram
+  stopTimer(zegar);  
+  printTimer(zegar); 
+  freeTimer(zegar);  
   cout << endl << "Wynik calkowania metoda trapezow: " << result << endl;
+}
+void readFile(){
+  ifstream infile(FILEPATH.c_str());
+  string n;
+  string fun;
+  getline(infile,n);
+  getline(infile,fun);
+  N = atoi(n.c_str());
+  FUN = fun;
 }
 
 int main(int argc, char** argv){
@@ -82,6 +103,10 @@ int main(int argc, char** argv){
     if (!strcmp(argv[i], "-f")) FILEPATH = argv[++i];
     if (!strcmp(argv[i], "-n")) N = atoi(argv[++i]);
     i++;
+  }
+  if(!FILEPATH.empty())
+  {
+    readFile();
   }
   rectangleRule();
   trapezoidalRule();
